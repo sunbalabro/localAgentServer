e769987807f64e6c965975c8b41dcb548a071d27
sunbalabro <sunbalabro786@gmail.com>
2024-01-13 11:12:40 +0500

    added / route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require("express")
 let app = express()
 let bodyparser  = require("body-parser");
+const PORT = process.env.PORT || 2000
 const mongoose = require("./db/models/connection");
 let cors = require("cors");
 const authrouter = require("./routes/auth");
@@ -8,5 +9,8 @@ app.use(cors())
 app.use(bodyparser.urlencoded({extended: false}))
 app.use(bodyparser.json());
 app.use("/auth", authrouter);
+app.get("/",(req, res)=>{
+    return res.json({success: true, message: "Server Running At PORT", PORT})
+})
 
-app.listen(2000)
+app.listen(PORT)
